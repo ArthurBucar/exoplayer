ec8a465a0f8c9a8a92e8e5374ab3f3e4c5eee61c
Arthur Bucar <arthur.bucar@gmail.com>
2022-07-22 14:22:17 -0300

    commit init

---
+++ app/src/main/java/com/arthur/exoplayer/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.arthur.exoplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.MediaMetadata
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.common.collect.ImmutableList
+
+class MainActivity : AppCompatActivity(), Player.Listener {
+
+    private lateinit var title: TextView
+    private lateinit var player: ExoPlayer
+    private lateinit var playerView: PlayerView
+    private lateinit var progress: ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        progress = findViewById(R.id.progress)
+        title = findViewById(R.id.title)
+
+        setUpPlayer()
+        addMp3Files()
+        addMP4Files()
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getInt("mediaItem") != 0) {
+                val restoredMediaItem = savedInstanceState.getInt("mediaItem")
+                val seekTime = savedInstanceState.getLong("SeekTime")
+                player.seekTo(restoredMediaItem, seekTime)
+                player.play()
+            }
+        }
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong("SeekTime", player.currentPosition)
+        outState.putInt("mediaItem", player.currentMediaItemIndex)
+    }
+
+    private fun setUpPlayer(){
+        player = ExoPlayer.Builder(this).build()
+        playerView = findViewById(R.id.video_view)
+        playerView.player = player
+        player.addListener(this)
+
+    }
+
+    private fun addMP4Files() {
+        val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+        val mediaItem2 = MediaItem.fromUri(getString(R.string.myTestMp4))
+        val newItems: List<MediaItem> = ImmutableList.of(
+            mediaItem,
+            mediaItem2
+        )
+        player.addMediaItems(newItems)
+        player.prepare()
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+
+        when(playbackState){
+            Player.STATE_BUFFERING -> {
+                progress.visibility = View.VISIBLE
+            }
+            Player.STATE_READY -> {
+                progress.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    private fun addMp3Files(){
+        val mediaItem = MediaItem.fromUri(getString(R.string.test_mp3))
+        player.addMediaItem(mediaItem)
+        player.prepare()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player.release()
+    }
+
+
+
+    override fun onMediaMetadataChanged(mediaMetadata: MediaMetadata) {
+        super.onMediaMetadataChanged(mediaMetadata)
+
+        title.text = mediaMetadata.title ?: mediaMetadata.displayTitle ?: "title not found"
+    }
+
+}
